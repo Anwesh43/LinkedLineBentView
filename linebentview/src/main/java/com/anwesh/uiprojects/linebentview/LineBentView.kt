2128fa623ae9b3887088bb28b2045946e768b71c
Anwesh43 <anweshthecool0@gmail.com>
2019-05-06 11:02:28 +0530

    adding LineBentView class

--- linebentview/src/main/java/com/anwesh/uiprojects/linebentview/LineBentView.kt
+++ linebentview/src/main/java/com/anwesh/uiprojects/linebentview/LineBentView.kt
@@ -59,3 +59,20 @@ fun Canvas.drawLBNode(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class LineBentView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

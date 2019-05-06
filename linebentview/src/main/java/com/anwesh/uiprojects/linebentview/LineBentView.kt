52d6fdb91236c4877ac3aea2421d788160073f18
Anwesh43 <anweshthecool0@gmail.com>
2019-05-06 11:08:40 +0530

    adding LBNode class

--- linebentview/src/main/java/com/anwesh/uiprojects/linebentview/LineBentView.kt
+++ linebentview/src/main/java/com/anwesh/uiprojects/linebentview/LineBentView.kt
@@ -119,8 +119,52 @@ class LineBentView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class LBNode(var i : Int, val state : State = State()) {
+
+        private var next : LBNode? = null
+        private var prev : LBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBNode {
+            var curr : LBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
